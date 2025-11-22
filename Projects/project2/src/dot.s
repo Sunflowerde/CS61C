5115eba6898fc6d57b043ac3e903379775250804
sunflwoer <ziwenxu@stu.pku.edu.cn>
2025-11-23 00:46:49 +0800

    Finish dot.s

--- Projects/project2/src/dot.s
+++ Projects/project2/src/dot.s
@@ -18,27 +18,35 @@
 #     this function terminates the program with error code 37
 # =======================================================
 dot:
-
-    # Prologue
-
-
+    ebreak # for debug
+    li t0, 0 # sum = 0
+    li t1, 0 # iterate for a1
+    li t2, 0 # iterate for a2
+    li t3, 1 # 1 for compare
+    li t4, 0 # step = 0   
+    blt a2, t3, exit1 
+    blt a3, t3, exit2
+    blt a4, t3, exit2
 loop_start:
-
-
-
-
-
-
-
-
-
-
-
-
+    beq t4, a2, loop_end # if step == a2, goto end
+    slli t5, t1, 2 # index1 * 4
+    slli t6, t2, 2 # index2 * 4
+    add t3, a0, t5 # address of a1[i]
+    add a5, a1, t6 # address of a2[i]
+    lw a6, 0(t3) # a1[i]
+    lw a7, 0(a5) # a2[i]
+    mul a7, a6, a7 # a1[i] * a2[i]
+    add t0, t0, a7
+    add t1, t1, a3 # index1 + stride1
+    add t2, t2, a4 # index2 + stride2
+    addi t4, t4, 1 # step++
+    j loop_start
 loop_end:
-
-
-    # Epilogue
-
-
+    addi a0, t0, 0
     jr ra
+exit1:
+    li a0, 36
+    j exit
+exit2:
+    li a0, 37
+    j exit
